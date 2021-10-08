660947d045a1d081324fc81bf412481d2dacdff8
Gary Roberts <tallstreet@gmail.com>
2021-10-08 19:06:35 +0100

    Add initial help me code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import emergencyNumbers from "./emergencyNumbers.json";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [words, setWords] = useState(undefined);
+  const [emergencyNumber, setEmergencyNumber] = useState(undefined);
+  const [helpNumber, setHelpNumber] = useState(undefined);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => {
+          window.what3words.api
+            .convertTo3wa({ lat: latitude, lng: longitude }, "en")
+            .then((response) => {
+              setWords(response.words);
+              setEmergencyNumber(emergencyNumbers[response.country]);
+              if (window.localStorage) {
+                window.localStorage.setItem("country", response.country);
+              }
+            });
+        }
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (window.localStorage) {
+      setHelpNumber(window.localStorage.getItem("helpNumber"));
+      setEmergencyNumber(
+        emergencyNumbers[window.localStorage.getItem("country")]
+      );
+    }
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      if (window.localStorage) {
+        window.localStorage.setItem("helpNumber", e.target.number.value);
+      }
+      setHelpNumber(e.target.number.value);
+      e.preventDefault();
+    },
+    [setHelpNumber]
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {helpNumber && (
+          <a className="button" href={`tel:${helpNumber}`}>
+            Call For Help
+          </a>
+        )}
+
+        {!helpNumber && (
+          <form name="helpNumber" onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="number"
+              placeholder="Enter number of friend"
+            />
+            <button type="submit">Save</button>
+          </form>
+        )}
+
+        {words && (
+          <div className="location">
+            <div className="w3wLogo">
+              <img alt="What3Words Location" src="w3w_logo.svg" />
+            </div>
+            <div>: {words}</div>
+          </div>
+        )}
+
+        {emergencyNumber && (
+          <a className="emergency_button" href={`tel:${emergencyNumber}`}>
+            Call Emergency Services
+          </a>
+        )}
       </header>
     </div>
   );
